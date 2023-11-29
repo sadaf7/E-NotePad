b89d3e8087d7f4fef9318e3fe47aa50d983c88ba
Sadaf Hossain <sadafhossain185@gmail.com>
2023-11-30 00:54:15 +0600

    implemented in redux and profile func added

--- backend/index.js
+++ backend/index.js
@@ -16,18 +16,18 @@ app.use(express.json());
 
 // ------Deployment-----
 
-// const _dirname1 = path.resolve()
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static(path.join(_dirname1,'/frontend/dist')))
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname1,'../frontend/dist')))
 
-//   app.get('*',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname1,"frontend","dist", "index.html"))
-//   })
-// } else{
-//   app.get('/',(req,res)=>{
-//     res.send('api is running')
-//   })
-// }
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"frontend","dist", "index.html"))
+  })
+} else{
+  app.get('/',(req,res)=>{
+    res.send('api is running')
+  })
+}
 
 // ------Deployment-----
 
